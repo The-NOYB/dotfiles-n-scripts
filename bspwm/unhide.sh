0e4f96c72e51617dd389c1eeac6063a065ea0915
Mehul Paul <106906881+The-NOYB@users.noreply.github.com>
2022-06-10 21:09:46 +0530

    Update unhide.sh

--- bspwm/unhide.sh
+++ bspwm/unhide.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-bspc query -N -n .hidden.window -d focused > ~/cs/.scripts/.hidden
-a=$(cat ~/cs/.scripts/.hidden | dmenu -sb "dark green")
+a=$(bspc query -N -n .hidden.window -d focused | dmenu -sb "dark green")
 bspc node "$a" -g hidden=off
-bspc node -f "$a"
-bspc query -N -n .hidden.window -d focused > ~/cs/.scripts/.hidden
+bspc node -f "$a
