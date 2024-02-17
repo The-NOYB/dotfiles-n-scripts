e2d9f47ba8dc949baeb454af7cf5152cee199f64
Mehul Paul <106906881+The-NOYB@users.noreply.github.com>
2024-02-17 14:58:37 +0530

    Add files via upload

---
+++ lemonbar/b2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+clock() {
+   date "+%e %B %Y, %H:%M" 
+}
+
+battery() {
+    status=$(cat /sys/class/power_supply/BAT1/status)
+    echo $(cat /sys/class/power_supply/BAT1/capacity)% \(${status:0:1}\) 
+}
+
+window(){
+    xprop -id `bspc query -N -n .window.focused` | awk '/^WM_CLASS/{print $4}'| tr -d \" | cut -c -40 
+}
+
+mem() {
+    free -h | awk '/^Mem:/{print $3"/"$2}'
+}
+
+temp() {
+    echo $(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))°C
+}
+
+d(){
+    bash desktop.sh
+}
+
+vol(){
+   amixer | grep -q 'off' && echo "off%{-u}" || echo "`amixer get Master | sed -n 's/^.*\[\([0-9]\+\)%.*$/\1/p'| uniq`%%{-u}" 
+}
+
+wifi(){
+   nmcli connection | sed -n 2p | cut --characters\=1-23
+}
+
+while  true
+do
+    BAR_INPUT="%{l}%{U#247948} %{F#23ba67}┃%{F}$(d) %{F#23ba67}┃┃ %{F} %{+u}$(window)%{-u}%{U} %{c} %{B#23ba67}%{F#1F1D19} $(clock) %{r}%{F}%{B}%{U#247948}%{F#23ba67} ┃ ↑↓ %{F}%{+u}$(wifi)%{-u} %{F#23ba67} ┃ λ %{F}%{+u}$(vol) %{F#23ba67} ┃ Δ %{F}%{+u}$(temp)%{-u} %{F#23ba67} ┃ Ξ %{F}%{+u}$(mem)%{-u} %{F#23ba67} ┃ ± %{F}%{+u}$(battery)%{-u} %{F#23ba67} ┃ %{F}%{U}%{F}%{A:~/cs/.scripts/off:}:~:%{A} %{}"
+    echo $BAR_INPUT
+    sleep 1
+done
+
