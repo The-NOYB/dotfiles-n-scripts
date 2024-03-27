33ad2c0c1d5395736d5f97c682b94270fd919168
Mehul <noyb@localhost.localdomain>
2024-03-27 22:30:01 +0530

    updated  .bashrc bspwm/ scripts/

--- scripts/b.sh
+++ scripts/b.sh
@@ -5,7 +5,8 @@ clock() {
 }
 
 battery() {
-    echo "± `cat /sys/class/power_supply/BAT1/capacity`% (`cat /sys/class/power_supply/BAT1/status`)" #echo -e "\uf242 Battery: 0" 
+    status=$(cat /sys/class/power_supply/BAT1/status)
+    echo $(cat /sys/class/power_supply/BAT1/capacity)% \(${status:0:1}\) 
 }
 
 window(){
@@ -17,7 +18,7 @@ mem() {
 }
 
 temp() {
-    acpi -t | awk '/^Thermal 1:/ {print $4"°"$6}'
+    echo $(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))°C
 }
 
 d(){
@@ -29,12 +30,12 @@ vol(){
 }
 
 wifi(){
-   nmcli dev | awk '/ connected/{print $1 }' 
+   nmcli connection | sed -n 2p | cut --characters\=1-23 | xargs
 }
 
 while  true
 do
-    BAR_INPUT="%{l}%{U#d63e66} ┃ $(d) ┃┃ %{} %{+u}$(window)%{-u}%{U} %{c}%{B#23ba67}%{F#000000} $(clock) %{F}%{B} %{r}%{F#51e2e2}%{U#51e2e2}┃ %{+u}↑↓ $(wifi)%{-u}%{U}%{F} %{F#f260de}%{U#f260de}┃ %{+u}λ $(vol) %{U}%{F}%{F#ef7f0e}%{U#ef7f0e}┃ %{+u}Δ $(temp)%{-u}%{U}%{F} %{F#00ff00}%{U#00ff00}┃ %{+u}Ξ $(mem)%{-u}%{U}%{F} %{F#efef0e}%{U#efef0e}┃ %{+u}$(battery)%{-u}%{U}%{F} %{F#d63e66}┃%{F} %{A:systemctl suspend:}%{A2:sudo reboot:}%{A3:sudo shutdown now:}:~:%{A}%{A}%{A} %{}"
+    BAR_INPUT="%{l}%{U#247948} %{F#23ba67}┃%{F}$(d) %{F#23ba67}┃┃ %{F} %{+u}$(window)%{-u}%{U} %{c} %{B#23ba67}%{F#1F1D19} $(clock) %{r}%{F}%{B}%{U#247948}%{F#23ba67} ┃ ↑↓ %{F}%{+u}$(wifi)%{-u} %{F#23ba67} ┃ λ %{F}%{+u}$(vol) %{F#23ba67} ┃ Δ %{F}%{+u}$(temp)%{-u} %{F#23ba67} ┃ Ξ %{F}%{+u}$(mem)%{-u} %{F#23ba67} ┃ ± %{F}%{+u}$(battery)%{-u} %{F#23ba67} ┃ %{F}%{U}%{F}%{A:~/cs/scripts/off:}:~:%{A} %{}"
     echo $BAR_INPUT
     sleep 1
 done
